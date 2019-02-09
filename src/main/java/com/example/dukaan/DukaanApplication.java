b0680a2536a7dc5cae4ce4ace775f7527b95b076
Avinash <mailavinashpratap@gmail.com>
2019-02-08 21:38:42 -0500

    Initial commit

---
+++ src/main/java/com/example/dukaan/DukaanApplication.java
@@ -0,0 +1,14 @@
+package com.example.dukaan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukaanApplication.class, args);
+	}
+
+}
+
